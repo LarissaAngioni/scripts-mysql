a75b41ec96d66b5e4c41589a3e26e7a5754a270d
Larissa Angioni <larissaangioni@gmail.com>
2024-07-16 16:30:18 -0300

    Correção

--- aula-2/atv-e-commerce.sql
+++ aula-2/atv-e-commerce.sql
@@ -11,15 +11,15 @@ estoque int not null,
 valor decimal(6,2) not null
 );
 
-insert into tb_produtos(nome_produto, descricao, categoria, valor) values
-("Capinha Moto G", "Capa para proteger smartphone", "Celulares", 20.00),
-("Fone de Ouvido", "Fone P2.", "Acessórios", 15.00),
-("Caixinha Bluetooth", "Caixa de som JBL, com bluetooth", "Aparelhos de som", 300.00),
-("WebCam", "Webcam logitech 1080p", "Perifericos", 200.00),
-("Mouse", "Mouse generico ubs", "Perifericos", 70.00),
-("Cabo type-c", "Cabo tipo c", "Cabos", 20.00),
-("Teclado sem fio Razer", "Teclado sem fio, com receptor", "Perifericos", 700.00),
-("Lúminaria de mesa", "Lúminaria de LED recarregável", "Outros", 60.00);
+insert into tb_produtos(nome_produto, descricao, categoria, estoque, valor) values
+("Capinha Moto G", "Capa para proteger smartphone", "Celulares", 100, 20.00),
+("Fone de Ouvido", "Fone P2.", "Acessórios", 50, 15.00),
+("Caixinha Bluetooth", "Caixa de som JBL, com bluetooth", "Aparelhos de som", 10, 300.00),
+("WebCam", "Webcam logitech 1080p", "Perifericos", 20, 200.00),
+("Mouse", "Mouse generico ubs", "Perifericos", 76, 70.00),
+("Cabo type-c", "Cabo tipo c", "Cabos", 200, 20.00),
+("Teclado sem fio Razer", "Teclado sem fio, com receptor", "Perifericos", 5, 700.00),
+("Lúminaria de mesa", "Lúminaria de LED recarregável", "Outros", 25, 60.00);
 
 select * from tb_produtos where valor > 500;
 
